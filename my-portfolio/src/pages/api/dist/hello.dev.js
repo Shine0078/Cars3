48e5a89a2ff62ea6e597d499b3f1306af4dce3d3
Shine0078 <128758030+Shine0078@users.noreply.github.com>
2024-12-17 06:16:15 -0500

    Add serverless function

--- my-portfolio/src/pages/api/dist/hello.dev.js
+++ my-portfolio/src/pages/api/dist/hello.dev.js
@@ -8,7 +8,7 @@ exports["default"] = handler;
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 function handler(req, res) {
   res.status(200).json({
-    name: "John Doe"
+    name: "Samuel Abraham"
   });
 }
 //# sourceMappingURL=hello.dev.js.map
